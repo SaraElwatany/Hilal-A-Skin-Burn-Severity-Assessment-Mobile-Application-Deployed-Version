fa36756cb44db4ee7f25f5065229308135e24c1d
Sara Elwatany <14712019100970@stud.cu.edu.eg>
2023-11-29 03:55:11 +0200

    Modify API to work on emulator

--- gp_app/lib/apis/apis.dart
+++ gp_app/lib/apis/apis.dart
@@ -8,7 +8,7 @@ fetch_login_info(String url) async {
 // A function that sends the username and password to the flask backend (return type as future object)
 Future<void> sendData(String username, String password) async {
   String url =
-      'http://127.0.0.1:19999/login'; // When launching the app 110.0.2.2:19999
+      'http://10.0.2.2:19999/login'; // When launching the app => 127.0.0.1:19999  For Emulator => 10.0.2.2:19999
 
   var response = await http.post(Uri.parse(url), body: {
     'username': username,
