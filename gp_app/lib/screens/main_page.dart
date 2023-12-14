62acf08904b1e1937697e462aa1aeb8c75832586
RawanFekry <rawanmohamedfekry@gmail.com>
2023-12-14 15:37:03 +0200

    Adding camera for taking or uploading the burm image

--- gp_app/lib/screens/main_page.dart
+++ gp_app/lib/screens/main_page.dart
@@ -1,69 +1,67 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:gp_app/generated/l10n.dart';
+import 'package:gp_app/screens/camera.dart';
 import 'package:gp_app/widgets/localization_icon.dart';
 
-class MainPageScreen extends StatefulWidget{
+class MainPageScreen extends StatefulWidget {
   const MainPageScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() { 
-    return  _MainPageState();
+  State<StatefulWidget> createState() {
+    return _MainPageState();
   }
 }
 
-
-class _MainPageState extends State <MainPageScreen>{
-
+class _MainPageState extends State<MainPageScreen> {
   @override
   Widget build(BuildContext context) {
-
-    return  Scaffold(
-      appBar: const LocalizationIcon(), 
-      body:  Padding(
-          padding: const EdgeInsets.all(100.0),
-          child: Column(
-            children: [
-              Center(
-                child: InkWell(
-                  onTap: (){
-            
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
+    return Scaffold(
+      appBar: const LocalizationIcon(),
+      body: Padding(
+        padding: const EdgeInsets.all(100.0),
+        child: Column(
+          children: [
+            Center(
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CameraScreen()),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
                       color: Theme.of(context).colorScheme.surface,
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                    height: 187,
-                    width: 187,
-                    child: Column(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                          const SizedBox(height: 10,),
-                        Image.asset('assets/images/flame.png',
-                        ),
-                        // const SizedBox(height: 10,)
-                        // ,
-                        Text(S.of(context).burns,
-                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                          color: Colors.white
-                        ),
-                        )
-                      ],
-                    ),
+                      borderRadius: BorderRadius.circular(10)),
+                  height: 187,
+                  width: 187,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Image.asset(
+                        'assets/images/flame.png',
+                      ),
+                      // const SizedBox(height: 10,)
+                      // ,
+                      Text(
+                        S.of(context).burns,
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleLarge!
+                            .copyWith(color: Colors.white),
+                      )
+                    ],
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
-      
-
-      
-
-
+      ),
     );
-    
   }
 }
