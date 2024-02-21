da09cbbe6b0548df25bbbdc94e2b612b33ec7252
Sara Elwatany <14712019100970@stud.cu.edu.eg>
2024-02-21 23:11:55 +0200

    ..

--- gp_app/lib/models/global.dart
+++ gp_app/lib/models/global.dart
@@ -3,6 +3,6 @@ int drId = 123;
 // Global Variable for User's Profession Detection from Login Screen
 String UserProfession = '';
 // Global Variable for User ID from Login Screen
-String userId = '';
+String userId = '0';
 // Global Variable for Burn ID from Capturing Image Screen
-String burnId = '';
+String burnId = '0';
